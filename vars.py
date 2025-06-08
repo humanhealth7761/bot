182580279132d41a0e8a0ca998c99fd91def3603
humanhealth7761 <humanhealth7761@gmail.com>
2025-06-08 15:08:41 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,9 +3,9 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "21705770"))
-API_HASH = environ.get("API_HASH", "3791d9c01fecf028b22e2812b0c8daed")
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "7813618750"))
+OWNER = int(environ.get("OWNER", ""))
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
